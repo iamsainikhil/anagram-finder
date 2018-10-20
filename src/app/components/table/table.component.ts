a5e774ceec47ce831c043e547d7f5e288c90dc5e
iamsainikhil <sainikhil.bheema@gmail.com>
2018-10-20 17:31:51 -0400

    Updated master level concept with favorites removed

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class TableComponent implements OnInit {
 
   @Input() data: any;
+  @Input() level: string;
   @Output() index: EventEmitter<Number> = new EventEmitter();
 
   constructor() { }
